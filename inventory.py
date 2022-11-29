363bb67657899e1c774d9e811212c8523af34a66
Matthew Suggars <42852733+HOUVA@users.noreply.github.com>
2022-11-28 19:56:07 -0700

    updated inventory, created test_inventory.py

--- inventory.py
+++ inventory.py
@@ -12,20 +12,22 @@ What about deleting items? ✔️
 """
 
 class StoreInventory: 
-    inventory_list = []
+    inventory_list = [
+        {'name': 'Milk', 'price': '2.29', 'quantity': '3'}
+    ]
     finished = False
+    command_options = ['HELP', 'DONE', 'ADD', 'DELETE', 'CLEAR', 
+                       'UPDATE', 'IMPORT', 'EXPORT', 'PRINT']
 
-    def __init__(self) -> None:
-        pass
 
     # Runs the app
     def start_app(self):
         while not self.finished:
             self.create_grid()
-            self.get_command()
+            self.validate_input()
         self.create_grid()
 
-    
+    # Creates a grid in the terminal
     def create_grid(self):
         print('     ---Inventory--- \n')
         print('Name      | Price  | Qty ')
@@ -37,32 +39,45 @@ class StoreInventory:
                       ' ' + item['quantity'] + '  |')
             print(self.sum_of_items())
         
-    # Takes user input and depending on the value, executes the correct method.
-    def get_command(self):
-
+    def validate_input(self): 
         command = input('\nCommand: ').upper()
+
+        if command in self.command_options:
+            self.command_manager(command)
+        else: 
+            print('Enter a valid input')
+        
+
+
+    # Takes user input and depending on the value, executes the correct method.
+    def command_manager(self, command):
         if command.__contains__('HELP'):
             self.help()
         if command.__contains__('DONE'):
             self.finished = True
         elif command.__contains__('DELETE'):
+            print(self.delete_item())
+        elif command.__contains__('CLEAR'):
             if self.inventory_list:
-                self.delete_item()
+                self.clear_items()
             else: 
-                print('')
+                print('Your list is already empty')
         elif command.__contains__('UPDATE'):
             if self.inventory_list:
                 self.update_item()
             else:
                 print('\nNo items to update\n')
         elif command.__contains__('ADD'):
-            self.inventory_list.append(self.add_item())
-        elif command.__contains__('PRINT'):
-            self.export_items()
+            self.add_item()
+        elif command.__contains__('EXPORT'):
+            answ = input('Type the file you would like to export to: ')
+            self.csv_file_writer(answ)
         elif command.__contains__('IMPORT'):
-            self.inventory_list = self.csv_file_reader()
-        else: 
-            print('Not a valid option, type help for more info')
+            answ = input('Type the file you would like to import: ')
+            self.inventory_list = self.csv_file_reader(answ)
+        elif command.__contains__('PRINT'):
+            self.print_items()
+
 
     # Support information on how the app works
     def help(self):
@@ -73,27 +88,26 @@ class StoreInventory:
     # creates a new dict object containing the name and price keys.
     # Item class creates the dict and can be called using create_dict_obj()
     def add_item(self): 
-        new_item = Item(name=input('Name: ').capitalize(), price=input('Price: '), quantity=input('Quantity: '))
+        name = input('Name: ').capitalize()
+        price = input('Price: ')
+        quantity = input('Quantity: ')
         checker = False
-
-        # looks for a copy of the new item in the exisiting list
-        # if a copy is found, tells the user to update the exisitng item
         for item in self.inventory_list:
             for key in item.keys():
-                if item[key] == new_item.name:
+                if item[key] == name:
                     checker = True
         if checker: 
             print('You already have that item in this List\nIf you want to update the current item, type update')
         else:
-            return new_item.create_dict_obj()
+            self.inventory_list.append(Item(name=name,price=price,quantity=quantity).create_dict_obj())
     
+
     # changes either the name or price of the object selected
     # input is case sensitive
     def update_item(self):
         input_val = input('Which item would you like to update?: ')
         changed_val_type = input('What would you like to update? (name/price/quantity/both): ').lower()
         new_val = input("New Value: ")
-
         for items in self.inventory_list:
             for key in items.keys(): 
                 if input_val in items[key]:
@@ -102,10 +116,27 @@ class StoreInventory:
 
     # Removes an Item dict object from the list, based on the user key input value
     def delete_item(self):
-        input_val = input('Which item would you like to delete?: ')
-        for item in self.inventory_list:
-            if input_val in item['name']:
-                del self.inventory_list[self.inventory_list.index(item)]
+        input_val = input('Which item would you like to delete?: ').capitalize()
+        if self.inventory_list:
+            for item in self.inventory_list:
+                try: 
+                    del self.inventory_list[self.inventory_list.index(item)]
+                except ValueError: 
+                    pass
+                """if input_val in item['name']:
+                    del self.inventory_list[self.inventory_list.index(item)]
+                    return 'Successfully deleted'
+                else:
+                    return 'Item not found in list'"""           
+        else:
+            return 'There is nothing to delete'
+        
+
+
+    # deletes all items in the list
+    def clear_items(self):
+        self.inventory_list = []
+
 
     # adds the price values for each dict object and displays the total at the 
     # bottom of the list
@@ -116,15 +147,15 @@ class StoreInventory:
         total = round(total, 2)
         return f'\nTotal: ${total}'
 
+
     # reads a csv file and returns a list of dictionaries
-    def csv_file_reader(self):
+    def csv_file_reader(self,answ):
         new_list = []
         new_dict_list = []
-        with open('inventory.csv', newline='') as csvfile:
+        with open(answ, newline='') as csvfile:
             invt_reader = csv.reader(csvfile, delimiter=',')
             for row in invt_reader:
                 new_list.append(row)
-
         for i in range(2,len(new_list)):
             new_dict_list.append({
                 'name': new_list[i][0],
@@ -134,26 +165,33 @@ class StoreInventory:
         return new_dict_list
     
 
-    def csv_file_writer(self):
-        pass
+    def csv_file_writer(self, answ):
+        with open(answ, 'w', newline='') as csvfile:
+            invt_writer = csv.writer(csvfile, delimiter=',')
+            invt_writer.writerow(['Inventory'])
+            invt_writer.writerow(['Name'] + ['Price'] + ['Quantity'])
+            for item in self.inventory_list:
+                invt_writer.writerow([item['name']] + [item['price']] + [item['quantity']])
+        print('Exported successfully')
 
 
     # exports the dict object to a txt file
-    def export_items(self):
+    def print_items(self):
         txt_file = open('inventory.txt', 'w')
         txt_file.write('        ---List---\n\n')
         for item in self.inventory_list:
                 txt_file.write(item['name'] + ' ' * (10 - len(item['name'])) + '|' + 
                       ' $' + item['price'] + '  |' + 
-                      ' ' + item['quantity'] + '  |')
-                txt_file.write(self.sum_of_items())
+                      ' ' + item['quantity'] + '  |\n')
+        txt_file.write(self.sum_of_items())
         print('printed successfully!\n')
           
 
 if __name__ == '__main__':
     inventory = StoreInventory()
     inventory.start_app()
-    inventory.csv_file_reader()
+    #inventory.csv_file_writer()
+    print(inventory.inventory_list)
 
 
 
--- test_inventory.py
+++ test_inventory.py
@@ -1,15 +1,16 @@
 import unittest
+from unittest import TestCase
+from unittest import mock
 import inventory
 
-class TestInventory(unittest.TestCase):
+class TestInventory(TestCase):
     def setUp(self) -> None:
         self.inventory = inventory.StoreInventory()
         self.inventory_list = inventory.StoreInventory().inventory_list
-        self.control_dict = {
-            'name': 'Beef',
-            'price': '2.29',
-            'quantity': '3'
-        }
+
+        self.control_dicts = [{ 'name': 'Beef', 'price': '2.29','quantity': '3'},
+                              {'name': 'Milk', 'price': '2.29', 'quantity': '2'}                 
+        ]
         self.control_list = [
             {'name': 'Milk', 'price': '2.29', 'quantity': '2'},
             {'name': 'Eggs', 'price': '3.49', 'quantity': '3'},
@@ -17,28 +18,29 @@ class TestInventory(unittest.TestCase):
             {'name': 'Bread', 'price': '$5.49', 'quantity': '4'}
         ]
 
-    # test the method will return a dictionary if the name is not 
-    # already in the list
-    def test_add_item_correct(self):
-        print('Enter correct input')
-        self.assertEqual(self.control_dict, self.inventory.add_item())
+    # validates method will return a dictionary if the name does not exist
+    @mock.patch('inventory.input', side_effect=['Beef', '2.29','3'])
+    def test_add_item(self, mocked_input):
+        self.inventory.add_item()
+        self.assertIn(self.control_dicts[0], self.inventory_list)
     
-    # checks method does not add dictionary to list if name already 
-    # in the list
-    def test_not_add_duplicate(self):
-        print('Enter incorrect item')
-        self.assertNotEqual(self.control_dict, self.inventory.add_item())
+    # validates method does not add dictionary to list if name already exists
+    #def test_not_add_duplicate(self):
+    #    print('Enter incorrect item')
+    #    self.assertNotEqual(self.control_dicts[0], self.inventory.add_item())
 
-    # checks the method properly deletes the item in the list
-    def test_delete_item(self):
-        deleted_dict = {'name': 'Milk', 'price': '2.29', 'quantity': '2'}
+    # validates method will remove the dictionary object from the self.inventory list
+    @mock.patch('inventory.input', return_value='Milk')
+    def test_delete_item(self, mocked_input):
+        self.inventory_list = self.control_list
         self.inventory.delete_item()
-        self.assertNotIn(deleted_dict, self.inventory_list)
+        self.assertNotIn(self.control_dicts[1], self.inventory_list)
 
-    # checks the method prints a message to 
-    def test_not_delete_item(self):
-        pass
-
+    # validates method will return a string if the input passed is not found in name key in list
+    @mock.patch('inventory.delete_item.input', side_effect='Milk')
+    def test_not_delete_item_not_in_list(self, mock_input):
+        self.inventory_list = []
+        self.assertEqual(self.inventory.delete_item(), 'Item not found in list')
 
 if __name__ == '__main__':
     unittest.main()
